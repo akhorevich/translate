abcf1e40863cf63033786350bc6a84b50469546a
Anastasiya Khorevich <a.khorevich@bssys.com>
2016-05-04 18:23:39 +0300

    add

---
+++ translate/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql.akhorevich.myjino.ru;dbname=akhorevich',
+    'username' => 'akhorevich',
+    'password' => 'ufolog',
+    'charset' => 'utf8',
+];
---
+++ translate/views/layouts/main.php
@@ -0,0 +1,73 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='https://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+<div class="up">
+<div class="block name">Рената Хамидуллина</div>
+<div class="block lang">Ru En De Sr</div>
+</div>
+    <?php
+    NavBar::begin([
+        'brandLabel' => false,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right nav-pills'],
+        'items' => [
+            ['label' => 'Главная', 'url' => ['/site/index']],
+            ['label' => 'Услуги и цены', 'url' => ['/site/about']],
+            ['label' => 'Статьи', 'url' => ['/site/blog']],
+            ['label' => 'Контакты', 'url' => ['/site/contact']],
+
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
